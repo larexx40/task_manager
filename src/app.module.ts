d8c7bd0d326120ae3b1efc8a471ba27ff4a9d6d2
larexx40 <larexx40@gmail.com>
2024-02-14 08:42:01 +0100

    done with user

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe  } from '@nestjs/common';
+import { APP_PIPE } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true,
+      })
+    }
+  ],
 })
 export class AppModule {}
